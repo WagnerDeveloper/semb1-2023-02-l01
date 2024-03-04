c1cbb83cc43108c1e887c937cedd5128ae511e7a
Wagner <wagnerjunio2102@gmail.com>
2024-03-04 13:38:35 -0300

    Ajuste Makefile

--- stm32f411-blackpill/src/.deps/startup.d
+++
@@ -1,1 +0,0 @@
-build/startup.o: startup.c
--- stm32f411-blackpill/src/main.c
+++ stm32f411-blackpill/src/main.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
+#include <stdint.h>
 
-int main(int argc, char *argv[])
+#define STM32_RCC_BASE 0x40023800
+#define STM32_GPIO_OFSET 0X30
+#define pRCC_AHB1ENR (STM32_RCC_BASE + STM32_GPIO_OFSET);
+
+int main(void)
 {
-  while(1){};
+  while (1)
 
+  {
+  };
   /* Nao deveria chegar aqui */
 
   return EXIT_SUCCESS;
-}
-
+};
